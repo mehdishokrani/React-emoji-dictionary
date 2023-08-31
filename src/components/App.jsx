9437639df1356224eafae33d4f03805b97ded33f
mehdishokrani <mehdishokrani1@gmail.com>
2023-08-31 01:58:31 -0600

    Final version of React APP that uses the map function

---
+++ src/components/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import emojipedia from "../emojipedia";
+import Emoji from "./Emoji";
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      <dl className="dictionary">
+      {emojipedia.map((emo, index) => (
+      <Emoji
+        emoji={emo.emoji}
+        name={emo.name}
+        meaning={emo.meaning}
+        id={emo.id}
+        key={index}
+      />
+      ))}
+      </dl>
+    </div>
+  );
+}
+
+export default App;
